eca90663f48d9a22a4d8d097b14a100cffab6fcc
stephieopiyo <stephieopiyo@gmail.com>
2021-11-12 13:03:42 +0300

    append

--- js/sight.js
+++ js/sight.js
@@ -2,10 +2,18 @@ class SVGElement {
     constructor(type) {
         this.type = type;
         this.namespace = 'http://www.w3.org/2000/svg';
-        this.node = document.createElementNS(namespace, type);
+        this.node = document.createElementNS(this.namespace, this.type);
         return this;
     }
     attr(attrs) {
+        for(const [key, value] of Object.entries(attrs)) {
+            this.node.setAttributeNS(null, key, value);
+        }
+        return this;
+
+    }
+    append(element) {
+        const parent = element.typeof === String ? document.querySelector(element) : element.node;
         return this;
     }
 }
